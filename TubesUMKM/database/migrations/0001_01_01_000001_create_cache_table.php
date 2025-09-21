d9e418cd21b05dd0c917010226a12809f187eb8d
MuhammadAzkaSubhan <azka.subhan05@gmail.com>
2025-09-21 19:53:33 +0700

    Membuat tabel tabel untuk database

--- TubesUMKM/database/migrations/0001_01_01_000001_create_cache_table.php
+++ TubesUMKM/database/migrations/0001_01_01_000001_create_cache_table.php
@@ -4,11 +4,9 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('cache', function (Blueprint $table) {
@@ -24,12 +22,11 @@ return new class extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
+   
     public function down(): void
     {
         Schema::dropIfExists('cache');
         Schema::dropIfExists('cache_locks');
     }
 };
+
