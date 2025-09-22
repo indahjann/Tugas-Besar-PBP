961b83e647250838e512337e9067d0c07ca3d418
indahjann <jannahindah7@gmail.com>
2025-09-22 16:15:58 +0700

    ini setup baru

--- TubesUMKM/database/migrations/0001_01_01_000002_create_jobs_table.php
+++ TubesUMKM/database/migrations/0001_01_01_000002_create_jobs_table.php
@@ -4,10 +4,11 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-
 return new class extends Migration
 {
-
+    /**
+     * Run the migrations.
+     */
     public function up(): void
     {
         Schema::create('jobs', function (Blueprint $table) {
@@ -44,7 +45,9 @@ return new class extends Migration
         });
     }
 
-
+    /**
+     * Reverse the migrations.
+     */
     public function down(): void
     {
         Schema::dropIfExists('jobs');
@@ -52,4 +55,3 @@ return new class extends Migration
         Schema::dropIfExists('failed_jobs');
     }
 };
-
