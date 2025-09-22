41c0da8a8a5ca8df2576ce52950b95ae8e0fad45
indahjann <jannahindah7@gmail.com>
2025-09-22 18:54:10 +0700

    database lengkap

--- TubesUMKM/routes/web.php
+++ TubesUMKM/routes/web.php
@@ -1,7 +1,40 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+// Register
+Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
+Route::post('/register', [AuthController::class, 'register']);
+
+// Login
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+
+// Logout
+Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+// Dashboard (protected)
+Route::get('/dashboard', function() {
+    return view('dashboard');
+})->middleware('auth')->name('dashboard');
+
+// Contoh route admin only
+Route::get('/admin', [AuthController::class, 'adminOnly'])
+    ->middleware('auth')
+    ->name('admin.dashboard');
