d278807f0b1803c2ddaa444bf8375c35e79168f8
MuhammadAzkaSubhan <azka.subhan05@gmail.com>
2025-10-19 10:30:12 +0700

    Menambahkan nama dan nim

--- TubesUMKM/resources/views/layouts/footer.blade.php
+++ TubesUMKM/resources/views/layouts/footer.blade.php
@@ -8,8 +8,11 @@
                     <div class="footer-logo-text">BUKUKU</div>
                 </div>
                 <div class="footer-description">
-                    Azka Indah Vino Dapa
-                </div>
+                    Irvino Kent Setiawan	- 24060123120008 <br>
+                    Indah Nurul Jannah		- 24060123120009 <br>
+                    Muhammad Azka Subhan 	- 24060123130086 <br>
+                    Muhammad Danendra Daffa	- 24060123140164
+            </div>
             </div>
         </div>
 
@@ -17,7 +20,7 @@
         <div class="footer-bottom">
             <div class="footer-bottom-content">
                 <div class="footer-copyright">
-                    © {{ date('Y') }} BUKUKU - Premium Books Collection. All rights reserved.
+                    © {{ date('Y') }} BUKUKU - Premium Books Collection.
                 </div>
             </div>
         </div>
